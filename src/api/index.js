a6882bc82f38abab872ebf5f34596a8daddf2b66
Ranjan08385 <moger.ranjan@gmail.com>
2021-03-22 11:36:30 +0530

    front end changes

---
+++ src/api/index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://project-tracker-activity.herokuapp.com/",
+});
+
+// const url = "http://localhost:5000/posts";
+
+// const url = "https://project-tracker-activity.herokuapp.com/posts";
+
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+
+  return req;
+});
+
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (newPost) => API.post("/posts", newPost);
+export const updatePost = (id, updatedPost) =>
+  API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+
+// Signin and SignUp Routes
+
+export const signIn = (formData) => API.post("/users/signin", formData);
+export const signUp = (formData) => API.post("/users/signup", formData);
